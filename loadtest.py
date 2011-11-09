169fc92f17663735ad64ba40083536ca1b72fa6f
xklakoux <artur@xklakoux.(none)>
2011-11-09 19:41:24 +0100

    Added example test and some minor changes

--- loadtest.py
+++ loadtest.py
@@ -10,13 +10,14 @@ import gen
 
 class LoadTest():
   def check_sb(self,cell,path,model):
-    print str(cell)
-    print str(path)
-    model[path][0] = not model[path][0]
-    return 
+    #self.treeview.get_selection().select_all()
+    print self.treeview.get_selection().count_selected_rows()
+    #for mod,sel in self.treeview.get_selection().get_selected_rows():
+      #mod[sel][0] = not mod[sel][0]
 
   def start_cb(self,widget,data=None):
     self.window.hide()
+
   def load_cb(self,widget,data=None):
     dialog = gtk.FileChooserDialog("Open..",None,gtk.FILE_CHOOSER_ACTION_OPEN,(gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL,gtk.STOCK_OPEN, gtk.RESPONSE_OK))
     dialog.set_default_response(gtk.RESPONSE_OK)
@@ -31,7 +32,6 @@ class LoadTest():
     filter.set_name("All files")
     filter.add_pattern("*")
     dialog.add_filter(filter)
-
 
     response = dialog.run()
     if response == gtk.RESPONSE_OK:
@@ -48,75 +48,74 @@ class LoadTest():
     elif response == gtk.RESPONSE_CANCEL:
       self.filename = None
     dialog.destroy()
-  def evented(self,widget,event):
-    if gtk.gdk.keyval_name(event.keyval) == 'space':
-      pass
-      pass
-      #self.check_sb(self.celltoggle,1,self.liststore)
+
+  def evented(self,model,iter,path):
+    #if gtk.gdk.keyval_name(event.keyval) == 'space':
+    self.check_sb(self.celltoggle, iter, self.liststore)
+    #iter = self.liststore.iter_next(iter)
+
   def make_table(self,words):
-    self.liststore = gtk.ListStore(gobject.TYPE_BOOLEAN,gobject.TYPE_STRING,gobject.TYPE_STRING)
+    self.liststore = gtk.ListStore(gobject.TYPE_BOOLEAN, gobject.TYPE_STRING, gobject.TYPE_STRING)
 
     for (first,two) in words:
-      self.liststore.append((True,first,two))
-
+      self.liststore.append((True, first, two))
+    self.iter = self.liststore.get_iter_first()
     self.treeview = gtk.TreeView(self.liststore)
     self.treeview.get_selection().set_mode(gtk.SELECTION_MULTIPLE)
+    self.treeview.set_rubber_banding(True)
     self.celltext = gtk.CellRendererText() 
     self.celltext2 = gtk.CellRendererText()
     self.celltoggle = gtk.CellRendererToggle()
-    self.celltoggle.set_property('activatable',True)
-    self.celltoggle.connect('toggled',self.check_sb,self.liststore)
+    self.celltoggle.set_property('activatable', True)
+    self.celltoggle.connect('toggled', self.check_sb, self.liststore)
     self.treeview.set_events(gtk.gdk.KEY_PRESS_MASK)
-    self.treeview.connect('key_press_event',self.evented)
+    self.treeview.connect('select-cursor-row', self.check_sb)
 
     self.tvcolumn1 = gtk.TreeViewColumn('Check')
-    self.tvcolumn1.pack_start(self.celltoggle,True)
-    self.tvcolumn1.add_attribute(self.celltoggle,'active',0)
+    self.tvcolumn1.pack_start(self.celltoggle, True)
+    self.tvcolumn1.add_attribute(self.celltoggle, 'active',0)
 
     self.treeview.append_column(self.tvcolumn1)
     self.tvcolumn2 = gtk.TreeViewColumn(self.words.col1)
-    self.tvcolumn2.pack_start(self.celltext,True)
-    self.tvcolumn2.add_attribute(self.celltext,'text',1)
+    self.tvcolumn2.pack_start(self.celltext, True)
+    self.tvcolumn2.add_attribute(self.celltext, 'text', 1)
 
     self.treeview.append_column(self.tvcolumn2)
     self.tvcolumn3 = gtk.TreeViewColumn(self.words.col2)
-    self.tvcolumn3.pack_start(self.celltext2,True)
-    self.tvcolumn3.add_attribute(self.celltext2,'text',2)
+    self.tvcolumn3.pack_start(self.celltext2, True)
+    self.tvcolumn3.add_attribute(self.celltext2, 'text', 2)
     self.treeview.append_column(self.tvcolumn3)
     self.scrolled = gtk.ScrolledWindow()
     self.scrolled.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
     self.scrolled.add_with_viewport(self.treeview)
-    self.window.vbox.pack_start(self.scrolled,True,True,10)
+    self.window.vbox.pack_start(self.scrolled, True, True, 10)
     self.window.show_all()
 
   def __init__(self,words):
 
-    self.filename = ''
-
     self.window = gtk.Dialog()
     self.window.set_modal(True)
-    self.window.set_size_request(500,500)
+    self.window.set_size_request(500, 500)
     self.window.set_border_width(10)
     self.window.set_resizable(False)
-    self.window.set_title('Editor')
-    self.window.connect('destroy',lambda wid:self.window.hide())
-    self.window.connect('delete_event',lambda a1,a2:self.window.hide())
+    self.window.set_title('Open')
+    self.window.connect('destroy', lambda wid:self.window.hide())
+    self.window.connect('delete_event', lambda a1,a2:self.window.hide())
     
-
-    box = gtk.HBox(False,0)
-    self.window.vbox.pack_start(box,False,False,10)
+    box = gtk.HBox(False, 0)
+    self.window.vbox.pack_start(box, False, False, 10)
     self.entry = gtk.Entry()
     self.entry.set_sensitive(False)
     self.entry.set_alignment(1.0)
     self.entry.show()
     
     self.loadbutton = gtk.Button('Load test')
-    self.loadbutton.connect('clicked',self.load_cb)
+    self.loadbutton.connect('clicked', self.load_cb)
     self.loadbutton.show()
     
     box.pack_start(self.entry,True,True,10)
     box.pack_end(self.loadbutton,False,False,10)
-
+    
     self.startbutton = gtk.Button('Start Test')
     self.startbutton.connect('clicked',self.start_cb)
     self.startbutton.set_sensitive(False)
